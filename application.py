34074460ee2e4a0004d7839ea1b51fade8035bd9
Sabine May <smay1@terpmail.umd.edu>
2023-10-21 18:13:11 -0400

    outline

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
 from flask import Flask
 import os
 import io
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from functools import reduce
+import nltk
+from translate import Translator
 
 app = Flask(__name__)
 
@@ -18,8 +24,40 @@ def get_URLs():
     
     return urls
 
+def get_text(url):
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, features="html.parser")
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+    # get text
+
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text.lower()
+
+def get_tuples(url):
+
+    text = get_text(url)
+    sections = nltk.word_tokenize(text)
+    tag = nltk.pos_tag(sections)
+
+    print(tag)
+
 def get_small_CSV(url, dict, freq, pos, lang): 
-    pass 
+    tuples = get_tuples(url)
+
+    # Filter out tuples with the wrong POS
+
+    # Sort tuples by count
+
+    # Cut list off based on freq
 
 def get_big_CSV(freq, pos, lang): 
     output = ""
